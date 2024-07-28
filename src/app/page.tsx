4d53a7948e06ba087b6a3c88b7328105d35cdc99
Tales Palma <tales.palma_2022@outlook.com>
2024-07-28 17:34:33 -0300

    Site mais responsivo e colocamos os links ja certinhos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,20 @@ export default function Home() {
   return (
     <div className={Style.container}>
       <header className={Style.header}>
+        <Card />
         <h1>Bem vindo ao meu site</h1>
+
+        {
+
+          //Colocar um apresentacao mais bonita e curto sobre mim aqui em cima
+          // Colocar uma parte para fazer download do meu CV
+          // Colocar uma apresentacao das tecnologias que uso e o tempo de experiencia
+        }
+
+
+
+
       </header>
-      <Card />
     </div>
   );
 }
