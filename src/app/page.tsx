083fed66c2f47531c0ec0fb4cbd5be9753663d5d
Tales Palma <tales.palma_2022@outlook.com>
2024-07-28 18:46:26 -0300

    Addicionamos mais infos da pagina além de um button onde será efetuado o dw do curriculo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,43 @@
 import { Card } from "@/components/card";
 import Style from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={Style.container}>
       <header className={Style.header}>
-        <Card />
-        <h1>Bem vindo ao meu site</h1>
-
-        {
-
-          //Colocar um apresentacao mais bonita e curto sobre mim aqui em cima
-          // Colocar uma parte para fazer download do meu CV
-          // Colocar uma apresentacao das tecnologias que uso e o tempo de experiencia
-        }
-
-
-
-
+        <h1>Seja bem-vindo!</h1>
+        <div className={Style.titleHeader}>
+          <Card />
+          <div className={Style.description}>
+            <h2>Quem sou eu?</h2>
+            <p>
+              Olá, meu nome é Tales Palma e sou um desenvolvedor Full Stack apaixonado por criar soluções inovadoras e eficientes.
+              Com uma sólida formação em Análise e Desenvolvimento de Sistemas, tenho experiência em diversas tecnologias e frameworks, o que me permite atuar em diferentes áreas do desenvolvimento de software.
+            </p>
+            <Link href="#" className={Style.buttonDownload}>Download CV</Link>
+          </div>
+        </div>
       </header>
-    </div>
-  );
+      <main className={Style.main}>
+        <div className={Style.divider}>        <h1>Tecnologias que domino</h1>
+          <ul>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Rust</li>
+            <li>Java</li>
+            <li>Spring</li>
+            <li>Rocket.rs</li>
+            <li>Next.js</li>
+            <li>React</li>
+            <li>React Native</li>
+            <li>Kotlin</li>
+            <li>Jetpack Compose</li>
+          </ul>
+        </div>
+      </main>
+      <footer className={Style.footer}>
+        <p>Desenvolvido por: <a href="https://github.com/talespalma">talespalma</a></p>
+      </footer>
+    </div>);
 }
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,5 +1,17 @@
 export default function Sobre() {
   return (
-    <div>Sobre</div>
+    <div>
+      <h2>Como eu trabalho?</h2>
+      <p>
+        Atualmente, trabalho como freelancer, focando no desenvolvimento de aplicativos móveis. Utilizo Kotlin tanto para desenvolvimento nativo quanto com Jetpack Compose, aproveitando o que há de mais moderno para criar interfaces intuitivas e funcionais.
+      </p>
+      <p>
+        No desenvolvimento web, minha expertise inclui o uso de React e Next.js para criar aplicações front-end dinâmicas e responsivas. Para o back-end, utilizo Rust com Rocket.rs para aplicações de alta performance e Java com Spring para soluções robustas e escaláveis.
+      </p>
+      <p>
+        Sou apaixonado por resolver problemas complexos e estou sempre em busca de novas tecnologias e melhores práticas para aprimorar minhas habilidades e entregar produtos de alta qualidade.
+      </p>
+      <h2>Porque dessas tecnologias?</h2>
+    </div>
   );
 }
